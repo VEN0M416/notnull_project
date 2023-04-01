3cd646db68443118585315f7b168037538265f64
TheKelp1e <vova_krasnov_2004@mail.ru>
2023-04-02 02:41:35 +0300

    zxc

---
+++ src/main/java/com/example/notnullproject/NotNullProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.notnullproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotNullProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotNullProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/notnullproject/NotNullProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notnullproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotNullProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
