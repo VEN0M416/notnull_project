bb4f81c411d8fa87490df0829e28d08660c575c0
VEN0M416 <amonullo.k.02@mail.ru>
2023-04-01 22:57:33 +0300

    форма, отправка данных в консоль

--- src/components/Forms/Modal.jsx
+++ src/components/Forms/Modal.jsx
@@ -1,4 +1,6 @@
-export default function Modal({ open, onClose, FormType, children}) {
+export default function Modal({ open, onClose, SendData, FormType, children}) {
+    const BtnStyle=' mx-3 rounded-[10px] py-2 px-12 bg-gradient-to-r from-lime-500 to-green-500 active:from-lime-700 active:to-green-700';
+    
     return (
         <div className={`fixed inset-0 ${open ? '' : 'pointer-events-none'}`}>
             {/* backdrop */}
@@ -8,13 +10,17 @@ export default function Modal({ open, onClose, FormType, children}) {
             />
  
             {/* content */}
-            <div className={`fixed top-5 left-1/3 rounded-[15px] bg-indigo-900 text-white shadow-lg w-full max-w-screen-sm ${open ? 'opacity-100' : 'pointer-events-none opacity-0'}`}>
-                <div className=' bg-indigo-950 flex justify-center rounded-t-[15px] py-4 text-3xl'>
+            <div className={`fixed top-[10%] left-1/3 rounded-[15px] bg-gradient-to-r from-cyan-700 to-blue-700  text-white shadow-lg w-full max-w-screen-sm ${open ? 'opacity-100' : 'pointer-events-none opacity-0'}`}>
+                <div className=' flex justify-center rounded-t-[15px] py-3 mt-4 text-3xl'>
                     {FormType}
                 </div>
                 { children }
-                <div className="flex justify-center mb-5">
-                    <button onClick={onClose} className=" mx-3 rounded-[10px] py-1 px-4 bg-hoverBg active:bg-activeBg">Close</button>
+                <div className="flex justify-center my-8 text-2xl">
+                    <button onClick={onClose} className={BtnStyle}>Close</button>
+                    <button onClick={()=>{
+                        onClose();
+                        SendData();
+                    }} className={BtnStyle}>Send</button>
                 </div>
             </div>
         </div>
--- src/components/Forms/SignUpForm.jsx
+++ src/components/Forms/SignUpForm.jsx
@@ -5,20 +5,34 @@ const InputStyle=' w-[80%] my-2 p-3 rounded-[10px] bg-gray-200 text-black placeh
 const InputPos='flex justify-center';
 
 export default function SignUpForm() {
-    const [ open, setOpen ] = useState(false)
+    const [ open, setOpen ] = useState(false);
+    const [data, setData] = useState({
+        name: '',
+        surname: '',
+        email: '',
+        phone: '',
+      });
+
+    const Send=(data)=>{
+        console.log(data);
+    }
+
     return (
         <div>
             <button onClick={() => setOpen(true)} className="hover:bg-hoverBg rounded-[10px] py-1 px-4 active:bg-activeBg">Sign Up</button>
-            <Modal open={open} onClose={() => setOpen(false)} FormType='Регистрация'>
+            <Modal open={open} onClose={() => setOpen(false)} SendData={() => Send(data)} FormType='Регистрация'>
                 <div className=' text-black my-4 '>
                     <div className={InputPos}>
-                        <input type="text" placeholder='Имя' className={InputStyle}/>
+                        <input type="text" placeholder='Имя' className={InputStyle} onChange={(e)=>{setData((prevSate)=>({...prevSate,name:e.target.value}))}}/>
                     </div>
                     <div className={InputPos}>
-                        <input type="text" placeholder='Фамилия' className={InputStyle}/>
+                        <input type="text" placeholder='Фамилия' className={InputStyle} onChange={(e)=>{setData((prevSate)=>({...prevSate,surname:e.target.value}))}}/>
                     </div>
                     <div className={InputPos}>
-                        <input type='email' placeholder='Почта' className={InputStyle}/>
+                        <input type='email' placeholder='Почта' className={InputStyle} onChange={(e)=>{setData((prevSate)=>({...prevSate,email:e.target.value}))}}/>
+                    </div>
+                    <div className={InputPos}>
+                        <input type='text' placeholder='Номер' className={InputStyle} onChange={(e)=>{setData((prevSate)=>({...prevSate,phone:e.target.value}))}}/>
                     </div>
                 </div>
             </Modal>
