3fbb167872b4652fce9db1a778c7b9a704e1c9d1
CAPVOK <capvok@yandex.ru>
2023-04-04 23:01:52 +0300

    NotFoundPage

--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -3,6 +3,7 @@ import  Main  from '../pages/Main.jsx';
 import Page1 from "../pages/Page1.jsx";
 import Page2 from "../pages/Page2.jsx";
 import Page3 from "../pages/Page3.jsx";
+import NotFoundPage from "../pages/NotFoundPage.jsx";
 
 
 function Router() {
@@ -13,6 +14,7 @@ function Router() {
         <Route path='/Page1' element={<Page1/>} />
         <Route path='/Page2' element={<Page2/> } />
         <Route path='/Page3' element={<Page3/> } />
+        <Route path="/*" element={<NotFoundPage/>}/>
       </Routes>
     </BrowserRouter>
   );
