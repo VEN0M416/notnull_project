f084262b281b83303003ac2cd22222ecadc828fc
CAPVOK <capvok@yandex.ru>
2023-04-05 00:47:20 +0300

    chat

--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -4,6 +4,7 @@ import Page1 from "../pages/Page1.jsx";
 import Page2 from "../pages/Page2.jsx";
 import Page3 from "../pages/Page3.jsx";
 import NotFoundPage from "../pages/NotFoundPage.jsx";
+import ChatPage from "../pages/ChatPage.jsx";
 
 
 function Router() {
@@ -15,6 +16,8 @@ function Router() {
         <Route path='/Page2' element={<Page2/> } />
         <Route path='/Page3' element={<Page3/> } />
         <Route path="/*" element={<NotFoundPage/>}/>
+
+        <Route path="/chat" element={<ChatPage/>}/>
       </Routes>
     </BrowserRouter>
   );
