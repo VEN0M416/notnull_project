041fcd5876120adab1ff13373122729485a05e46
TheKelp1e <vova_krasnov_2004@mail.ru>
2023-04-07 01:45:44 +0300

    WebSocket tests

--- src/main/java/com/example/notnullproject/chat/configuration/WebSocketConfig.java
+++ src/main/java/com/example/notnullproject/chat/configuration/WebSocketConfig.java
@@ -31,8 +31,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("*");
     }
 
     @Override
