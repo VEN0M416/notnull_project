a8b1e16c481ad3fd06ef40d47b1ccc4018de9417
VEN0M416 <amonullo.k.02@mail.ru>
2023-04-11 01:42:55 +0300

    графики на page2

--- src/components/pages/Page2.jsx
+++ src/components/pages/Page2.jsx
@@ -1,15 +1,39 @@
 import Header from '../Header/Header'
+import { useState } from "react";
+import { Data } from '../charts/Data';
+import LineChart from '../charts/LineChart';
+
 
 const Page2 = () =>{
-    return (
-      <>
-        <Header/>
-        <div className='container mx-auto text-white'>
+  const [chartData, setChartData] = useState({
+    labels: Data.map((data)=>data.year),
+    datasets: [{
+        label: "users gained",
+        data: Data.map((data)=>data.userGain),
+        tension: 0.4,
+        /* fill: 1 */
+      },{
+        label: "users losted",
+        data: Data.map((data)=>data.userLost),
+        tension: 0.4,
+        /* fill: true */
+      }
+    ]
+  });
 
-          <p className='text-2xl pt-[50px]'>Page2</p>
-          
+  return (
+    <>
+      <Header/>
+      <div className='container mx-auto text-white'>
+
+        <p className='text-2xl pt-[50px]'>Page2</p>
+
+        <div className='bg-white my-5 p-5 rounded-lg h-[50%] w-[50%]'>
+          <LineChart chartData={chartData} />
         </div>
-      </>
-    );
-  }
-  export default Page2;
+
+      </div>
+    </>
+  );
+}
+export default Page2;
