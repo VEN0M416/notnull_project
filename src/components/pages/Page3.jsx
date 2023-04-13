5ead641be07b2b08dedb1d48b7f0648bd808b959
VEN0M416 <amonullo.k.02@mail.ru>
2023-04-13 04:11:35 +0300

    комит для Родиона в page3

--- src/components/pages/Page3.jsx
+++ src/components/pages/Page3.jsx
@@ -1,10 +1,9 @@
 import Header from '../Header/Header'
-import { Tab } from '@headlessui/react'
-import GenderSelect from '../GenderSelect/GenderSelect'
+import GenderSelect from '../GenderSelect/GenderSelect';
+import { Tab } from '@headlessui/react';
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
+const tabStyle = "my-1 px-3 py-2 text-gray-400 leading-5 rounded-md focus:outline-none focus:text-white focus:bg-gray-700 hover:text-white hover:bg-gray-700";
+const inputStyle = "my-1 bg-white/20 rounded placeholder:text-gray-400 text-white px-3 py-1";
 
 const Page3 = () =>{
 
@@ -12,110 +11,130 @@ const Page3 = () =>{
     <>
       <Header/>
       <div className='container mx-auto text-black'>
-
         <p className='text-2xl pt-[50px] text-white'>Page3</p>
-
-        <div className=" mt-5">
+        <div className=" container mx-auto text-md mt-5 bg-blue-950/50 backdrop-blur-sm rounded-md h-[700px]">
           <Tab.Group >
             <div className='flex justify-center flex-wrap sm:flex-nowrap'>
-              <div className=' whitespace-nowrap rounded-xl bg-blue-900/20 p-1 w-5/12 h-max mx-1'>
+              <div className=' ms:whitespace-nowrap rounded-xl p-1 w-5/12 h-max mx-1 text-lg'>
                 <Tab.List className="flex flex-col ">
-                  <Tab className="mt-1 px-3 py-2 text-gray-400 text-sm font-medium leading-5 rounded-md focus:outline-none focus:text-white focus:bg-gray-700 hover:text-white hover:bg-gray-700">
+                  <Tab className={tabStyle}>
                     Личные данные
                   </Tab>
-                  <Tab className="mt-1 px-3 py-2 text-gray-400 text-sm font-medium leading-5 rounded-md focus:outline-none focus:text-white focus:bg-gray-700 hover:text-white hover:bg-gray-700">
+                  <Tab className={tabStyle}>
                     Безопасность и пароли
                   </Tab>
-                  <Tab className="mt-1 px-3 py-2 text-gray-400 text-sm font-medium leading-5 rounded-md focus:outline-none focus:text-white focus:bg-gray-700 hover:text-white hover:bg-gray-700">
+                  <Tab className={tabStyle}>
                     Банковские реквизиты
                   </Tab>
                 </Tab.List>
               </div>
               <Tab.Panels className=" w-7/12">
                 <Tab.Panel >
-                  <div className="flex text-gray-400 mx-1">
-                    <div className="mx-2 items-center justify-center">
+                  <div className="flex text-white mx-1">
+                    <div className="mx-3 items-center justify-center">
                       <p>Имя</p>
-                      <input placeholder='Имя'className='bg-white/20 rounded placeholder:text-white text-white px-3 py-1' defaultValue="Initial value"></input>
-                      
+                      <input 
+                        placeholder='Имя'
+                        className={inputStyle}
+                      />
                       <p>Пол</p>
-                      <GenderSelect/>
-                      
+                      <GenderSelect/> 
                       <p>Никнейм</p>
-                      <input placeholder="никнейм" className='bg-white/20 rounded placeholder:text-white text-white px-3 py-1' defaultValue="Initial value"></input>
+                      <input 
+                        placeholder="Никнейм" 
+                        className={inputStyle}
+                      />
                       <br/>
-                      <button className="my-3 border px-1 rounded"> Сохранить</button>
+                      <button 
+                        className="my-3 border px-4 py-1 rounded text-white"
+                        /* onClick={} */
+                      > Сохранить</button>
                     </div>
                     <div className="mx-3">
                       <p>Фамилия</p>
-                        <input placeholder='Фамилия' className='bg-white/20 rounded placeholder:text-white text-white px-3 py-1' defaultValue="Initial value"></input>
-                        <span></span>
+                      <input 
+                        placeholder='Фамилия' 
+                        className={inputStyle}
+                      />
                       <p>Дата рождения</p>
-                      <input type="datetime-local" id="meeting-time"
-                        name="meeting-time" defaultValue="2003-03-1215:32"
-                        className='bg-white/20 rounded placeholder:text-white text-white px-3 py-1'
-                      ></input>
+                      <input 
+                        type="date" 
+                        id="meeting-date"
+                        name="meeting-date"
+                        className={inputStyle+" dark:[color-scheme:dark]"}
+                      />
                     </div>
                   </div>
                 </Tab.Panel>
                 <Tab.Panel >
-                  <div className="text-gray-400 border color-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" for="email">
-                          Электронная почта
-                        </label>
-                        <input
-                          className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                          id="email"
-                          type="email"
-                          placeholder="example@example.com"
-                          defaultValue="Initial value"
-                        ></input>
-                        <button className="mx-5 px-3 border color-white rounded"> Сохранить</button>
+                  <div className="text-white mx-1 rounded">
+                    <div className="my-2 mx-3">
+                      <p>Электронная почта</p>
+                      <input
+                        className={inputStyle}
+                        id="email"
+                        type="email"
+                        placeholder="example@example.com"
+                      ></input>
+                      <p>Пароль</p>
+                      <input
+                        className={inputStyle}
+                        id="password"
+                        type="password"
+                        placeholder="Enter password"
+                      />
+                      <p>Телефон</p>
+                      <input
+                        className={inputStyle}
+                        id="phone"
+                        type="tel"
+                        placeholder="Введите номер телефона"
+                      />
+                      <br />
+                      <button 
+                        className="my-3 border px-4 py-1 rounded text-white"
+                        /* onClick={} */
+                      > Сохранить</button>
                     </div> 
-                    <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
-                      Пароль
-                    </label>
-                    <input
-                      className="shadow appearance-none border rounded  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      id="password"
-                      type="password"
-                      placeholder="********"
-                      defaultValue="Initial value"
-                    />
-                    <button className="mx-5 px-3 border color-white rounded"> Сохранить</button>
-                  <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2 py-2" for="phone">
-                      Телефон
-                    </label>
-                    <input
-                      className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      id="phone"
-                      type="tel"
-                      placeholder="Введите номер телефона"
-                      defaultValue="Initial value"
-                    />
-                    <button className="mx-5 px-3 border color-white rounded"> Сохранить</button>
-                  </div>                
                   </div>
                 </Tab.Panel>
                 <Tab.Panel>
-                  <div className="text-white my-3 mx-3">
-                  <input
-                      className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  <div className="text-white my-3 mx-3 rounded">
+                    <p>Карта</p>
+                    <input
+                      className={inputStyle}
                       id="card"
                       type="tel"
                       placeholder="Введите номер карты"
-                      defaultValue="Initial value"
                     />
-                    <button className="mx-5 px-3 border color-white rounded"> Подтвердить</button>
+                    <div className='flex space-x-5'>
+                      <div>
+                        <p>Месяц / Год</p>
+                        <input 
+                          type="text" 
+                          className={inputStyle+" w-[100px]"} 
+                          placeholder="MM/YYYY"
+                        />
+                      </div>
+                      <div>
+                      <p>CVV</p>
+                        <input 
+                          type="password" 
+                          className={inputStyle+" w-[60px]"} 
+                          placeholder="CVV"
+                        />
+                      </div>
+                    </div>
+                    <button 
+                      className="px-3 border color-white rounded my-2 py-1"
+                      /* onClick={} */
+                    > Подтвердить</button>
                   </div>
                 </Tab.Panel>
               </Tab.Panels>
             </div>
           </Tab.Group>
         </div>
-        
       </div>
     </>
   );
