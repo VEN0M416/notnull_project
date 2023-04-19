34198562ea156f6b18c0a6cbd5ef7a0dfd31ef8c
CAPVOK <capvok@yandex.ru>
2023-04-19 21:06:05 +0300

    поиск

--- src/components/charts/MainData.js
+++ src/components/charts/MainData.js
@@ -155,3 +155,16 @@ export const Data1 = [
         ],  
     },
 ]
+
+export const DataShares = [
+    { name: "Apple Inc.", symbol: "AAPL", price: "134.16", change: 0.64, percentChange: 0.48 },
+    { name: "Microsoft Corporation", symbol: "MSFT", price: "258.74", change: -0.59, percentChange: -0.23 },
+    { name: "Procter & Gamble Co", symbol: "PG", price: "134.35", change: 0.44, percentChange: 0.33 },
+    { name: "Amazon.com, Inc.", symbol: "AMZN", price: "3,187.75", change: -20.22, percentChange: -0.63 },
+    { name: "Alphabet Inc. Class A", symbol: "GOOGL", price: "2,268.30", change: -6.23, percentChange: -0.27 },
+    { name: "Johnson & Johnson", symbol: "JNJ", price: "165.07", change: 0.33, percentChange: 0.20 },
+    { name: "Facebook, Inc.", symbol: "FB", price: "314.63", change: -0.53, percentChange: -0.17 },
+    { name: "Tesla, Inc.", symbol: "TSLA", price: "747.92", change: -10.00, percentChange: -1.32 },
+    { name: "Netflix, Inc.", symbol: "NFLX", price: "590.35", change: 1.31, percentChange: 0.22 },
+    { name: "Walt Disney Co", symbol: "DIS", price: "186.81", change: 0.20, percentChange: 0.11 },
+  ];
--- src/components/pages/StocksPage.jsx
+++ src/components/pages/StocksPage.jsx
@@ -1,31 +1,25 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Header from '../Header/Header'
-import { click } from '@testing-library/user-event/dist/click';
+import { DataShares } from '../charts/MainData';
 
 const StocksPage = () => {
   const navigate = useNavigate();
   const [searchText, setSearchText] = useState("");
+  const [stocks, setStocks] = useState(DataShares);
 
   const clickTable = (symbol) => {
     navigate(`/stocks/${symbol}`);
   }
 
-
-  const stocks = [
-    { name: "Apple Inc.", symbol: "AAPL", price: "134.16", change: 0.64, percentChange: 0.48 },
-    { name: "Microsoft Corporation", symbol: "MSFT", price: "258.74", change: -0.59, percentChange: -0.23 },
-    { name: "Procter & Gamble Co", symbol: "PG", price: "134.35", change: 0.44, percentChange: 0.33 },
-    { name: "Amazon.com, Inc.", symbol: "AMZN", price: "3,187.75", change: -20.22, percentChange: -0.63 },
-    { name: "Alphabet Inc. Class A", symbol: "GOOGL", price: "2,268.30", change: -6.23, percentChange: -0.27 },
-    { name: "Johnson & Johnson", symbol: "JNJ", price: "165.07", change: 0.33, percentChange: 0.20 },
-    { name: "Facebook, Inc.", symbol: "FB", price: "314.63", change: -0.53, percentChange: -0.17 },
-    { name: "Tesla, Inc.", symbol: "TSLA", price: "747.92", change: -10.00, percentChange: -1.32 },
-    { name: "Netflix, Inc.", symbol: "NFLX", price: "590.35", change: 1.31, percentChange: 0.22 },
-    { name: "Walt Disney Co", symbol: "DIS", price: "186.81", change: 0.20, percentChange: 0.11 },
-  ];
+  /* Поиск / сортировка */
+  useEffect(()=>{
+    setStocks(DataShares.filter((item)=>
+      item.name.toLowerCase().indexOf(searchText.toLowerCase()) === 0 || item.symbol.toLowerCase().indexOf(searchText.toLowerCase()) === 0
+    ));
+  },[searchText]);
 
   return (
     <>
@@ -35,7 +29,7 @@ const StocksPage = () => {
         {/* Список акций */}
         <div className="bg-gradient-to-tr from-white/10 to-white/20 backdrop-blur-md text-white rounded-lg shadow-md p-6">
           
-          <div className='relative'>
+          <div className='w-6/12'>
             <input type="text" value={searchText} 
               onChange={(e)=>setSearchText(e.target.value)} placeholder="Название или тикер"
               className='bg-white p-4 w-full rounded-full text-black focus:outline-none'
@@ -44,19 +38,19 @@ const StocksPage = () => {
           
           <h2 className="mt-8 text-xl font-bold mb-4">Акции</h2>
           <table className="w-full table-auto">
-            <thead>
+            <thead> 
               <tr className="border-b-2 border-gray-200">
                 <th className="text-left pb-2">Название</th>
                 <th className="text-right pb-2">Цена</th>
                 <th className="text-right pb-2 " >Изменение цены</th>
               </tr>
             </thead>
-            <tbody>
+            <tbody>{/* Все акции */}
               {stocks.map((stock) => (
                 <tr onClick={()=>{clickTable(stock.symbol)}} key={stock.symbol} className=" border-t-[1px] border-gray-200 hover:bg-white/10">
                   <td className="py-2 grid grid-rows">
                       <div>{stock.name}</div>
-                      <div className='text-sm'>{stock.symbol}</div>
+                      <div className='text-sm font-light'>{stock.symbol}</div>
                   </td>
                   <td className="py-2 text-right">{parseFloat(stock.price.replace(/,/g, '')).toFixed(2)} $</td>
                   <td className='py-2 grid grid-rows'>
